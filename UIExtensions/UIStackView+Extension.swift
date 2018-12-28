d94548b7a157e542c0f4a9833cea57eea2c2a51f
Jen Chuah <jenmin1314@gmail.com>
2018-12-28 22:02:57 +1030

    remove all subview from uistack

---
+++ UIExtensions/UIStackView+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIStackView+Extension.swift
+//  ShopifyMobileApp
+//
+//  Created by Jen Min Chuah on 20/12/18.
+//  Copyright © 2018 Jenuine Apps. All rights reserved.
+//
+
+extension UIStackView {
+    func removeAllArrangedSubviews() {
+        
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+        
+        // Deactivate all constraints
+        NSLayoutConstraint.deactivate(removedSubviews.flatMap({ $0.constraints }))
+        
+        // Remove the views from self
+        removedSubviews.forEach({ $0.removeFromSuperview() })
+    }
+}
